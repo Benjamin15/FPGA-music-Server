af252a026d9dd00d33313823ae0b84e2443d7627
Benjamin <benjamin150594@hotmail.fr>
2018-10-28 19:53:27 -0400

    merge

--- src/player/player.cpp
+++ src/player/player.cpp
@@ -1,25 +1,4 @@
-<<<<<<< HEAD
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/mman.h>
-#include <mad.h>
-#include <pulse/simple.h>
-#include <pulse/error.h>
-
-pa_simple *device = NULL;
-int ret = 1;
-int error;
-struct mad_stream mad_stream;
-struct mad_frame mad_frame;
-struct mad_synth mad_synth;
-
-void output(struct mad_header const *header, struct mad_pcm *pcm);
-
-=======
 #include "player.h"
->>>>>>> 6840440f5a8f619dfef525e1a952cb606b67dce9
 int main(int argc, char **argv) {
 
     initPlayer();
@@ -195,48 +174,4 @@ void free_mad() {
     mad_synth_finish(&mad_synth);
     mad_frame_finish(&mad_frame);
     mad_stream_finish(&mad_stream);
-<<<<<<< HEAD
-
-    // Close PulseAudio output
-    if (device)
-        pa_simple_free(device);
-
-    return EXIT_SUCCESS;
-}
-
-// Some helper functions, to be cleaned up in the future
-int scale(mad_fixed_t sample) {
-     /* round */
-     sample += (1L << (MAD_F_FRACBITS - 16));
-     /* clip */
-     if (sample >= MAD_F_ONE)
-         sample = MAD_F_ONE - 1;
-     else if (sample < -MAD_F_ONE)
-         sample = -MAD_F_ONE;
-     /* quantize */
-     return sample >> (MAD_F_FRACBITS + 1 - 16);
-}
-void output(struct mad_header const *header, struct mad_pcm *pcm) {
-    register int nsamples = pcm->length;
-    mad_fixed_t const *left_ch = pcm->samples[0], *right_ch = pcm->samples[1];
-    static char stream[1152*4];
-    if (pcm->channels == 2) {
-        while (nsamples--) {
-            signed int sample;
-            sample = scale(*left_ch++);
-            stream[(pcm->length-nsamples)*4 ] = ((sample >> 0) & 0xff);
-            stream[(pcm->length-nsamples)*4 +1] = ((sample >> 8) & 0xff);
-            sample = scale(*right_ch++);
-            stream[(pcm->length-nsamples)*4+2 ] = ((sample >> 0) & 0xff);
-            stream[(pcm->length-nsamples)*4 +3] = ((sample >> 8) & 0xff);
-        }
-        if (pa_simple_write(device, stream, (size_t)1152*4, &error) < 0) {
-            fprintf(stderr, "pa_simple_write() failed: %s\n", pa_strerror(error));
-            return;
-        }
-    } else {
-        printf("Mono not supported!");
-    }
-=======
->>>>>>> 6840440f5a8f619dfef525e1a952cb606b67dce9
 }
