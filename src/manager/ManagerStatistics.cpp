b150fb1540374bc4c01d1119a5fa466909a3fb6e
Benjamin <benjamin150594@hotmail.fr>
2018-11-20 18:00:00 -0500

    fonction de statistique

--- src/manager/ManagerStatistics.cpp
+++ src/manager/ManagerStatistics.cpp
@@ -1,5 +1,77 @@
 #include "ManagerStatistics.h"
 
-void ManagerStatistics::get_statistics(const std::shared_ptr< restbed::Session > session){
-  std::cout << "statistiques" << std::endl;
+/**
+ * reset stats each 24 hours
+ * 
+ */ 
+void reset() {
+  users.clear();
+  musics.clear();
+  n_music_remove_admin = 0;
+}
+
+/**
+ * add new user when they add a music, for each day
+ * 
+ */ 
+void add_user(User user) {
+  if(std::find(users.begin(), users.end(), user) == users.end()) {
+     mutex.lock();
+    users.push_back(user);
+    mutex.unlock();
+  } 
+}
+
+/**
+ * add a new music for each day
+ * 
+ */ 
+void add_music(Music music) {
+  mutex.lock();
+  musics.push_back(music);
+  mutex.unlock();
+}
+
+/**
+ * we need to increase the number of music remove by the admin. Reset at each day
+ * 
+ */ 
+void new_remove_music() {
+  n_music_remove_admin++;
+}
+
+
+/**
+ * get count user
+ * 
+ */ 
+int get_n_users() {
+  return users.size();
+}
+
+/**
+ * get count music
+ * 
+ */ 
+int get_n_musics() {
+  return musics.size();
+}
+
+/**
+ * get number of music remove by the admin
+ * 
+ */ 
+int get_n_music_remove_admin() {
+  return n_music_remove_admin;
+}
+
+/**
+ * get the average time of music. Reset at each day
+ * 
+ */ 
+int get_avg_time() {
+  /*for (Music music : musics) {
+    music.duration_
+  }*/
+  return 0;
 }
--- src/manager/ManagerStatistics.h
+++ src/manager/ManagerStatistics.h
@@ -3,14 +3,22 @@
 #include <memory>
 #include <restbed>
 #include <iostream>
+#include <vector>
+#include <mutex>
 
-/**
- * Class which manage all users
- */  
+#include "../model/User.h"
+#include "../model/Music.h"
 
-class ManagerStatistics
-{  
-  private: 
-  public:
-    static void get_statistics(const std::shared_ptr< restbed::Session > session);
-};
+void reset();
+void add_user(User user);
+void add_music(Music music);
+void new_remove_music();
+int get_n_users();
+int get_n_musics();
+int get_n_music_remove_admin();
+int get_avg_time();
+
+static std::vector<User> users;
+static std::vector<Music> musics;
+static int n_music_remove_admin = 0;
+static std::mutex mutex;
