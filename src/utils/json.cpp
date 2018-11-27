7d1b03061c8fbed71afcdcc229a055dbcdde8365
Wassim Guellati <MW-Guellati@outlook.fr>
2018-11-27 14:44:23 -0800

    fix response statistics

--- src/utils/json.cpp
+++ src/utils/json.cpp
@@ -114,9 +114,9 @@ std::string getListForAdmin(std::vector<Music> musics) {
  */ 
 std::string getStats(int n_music, int n_user, int n_music_remove, std::string avg_duration) {
   std::stringstream result;
-  result << "{ \"chansons\": " << n_music << "\n";
-  result << "\"utilisateurs\": " << n_user << "\n";
-  result << "\"elemines\": " << n_music_remove << "\n";
+  result << "{ \"chansons\": " << n_music << "\n,";
+  result << "\"utilisateurs\": " << n_user << "\n,";
+  result << "\"elemines\": " << n_music_remove << "\n,";
   result << "\"temps\": \"" << avg_duration << "\" \n";
   result << "}";
   return result.str(); 
